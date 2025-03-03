c69159434415d0dc790ecc0a3bcc32d17fbb7730
Vitaly <starikovv98@gmail.com>
2025-03-03 17:53:45 +0400

    clean up and sort out imports

--- pta_learn/bourdet_derivative.py
+++ pta_learn/bourdet_derivative.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 from bisect import bisect_right
 from .superposition_calculation import *
 
--- pta_learn/ti_workflow.py
+++ pta_learn/ti_workflow.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pandas as pd
-
 from .tpmr import *
 from .lmir import *
 
